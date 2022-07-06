851de642ed9a0d9848da3dcaeb2c7931dee631e8
Rashik Shrestha <rashikshrestha99@gmail.com>
2022-07-06 11:41:44 +0545

    commit

--- main.js
+++ main.js
@@ -18,11 +18,33 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-const isogrps = document.querySelectorAll(".iso");
+// const isogrps = document.querySelectorAll(".iso");
 
-isogrps.forEach(iso => {
-    iso.addEventListener("click", () => {
-        iso.classList.toggle("active");
-    })
-})
+// isogrps.forEach(iso => {
+//     iso.addEventListener("click", () => {
+//         iso.classList.toggle("active");
+//     })
+// })
+
+var acc = document.getElementsByClassName("accordion");
+var panel = document.getElementsByClassName('panel');
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        var setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+}
+
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
+}
 
